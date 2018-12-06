163c2f9484006e9470c3b630914e9f51ecbf0f5f
Your Name <ilin96inbox@gmail.com>
2018-12-06 11:09:21 +0800

    api

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
-export const proxy = 'https://cors-anywhere.herokuapp.com/';
-export const key = '462b1cc8d4f2730081462fbc65136320';
+export const proxy = "https://cors-anywhere.herokuapp.com/";
+export const key = "6c676c37a32dbda3dc1a2d38ff70b07e";
